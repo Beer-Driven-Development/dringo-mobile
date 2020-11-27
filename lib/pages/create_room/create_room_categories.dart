77e872e3464c891d549170363488743de3967803
Szymon Skrzypacz <skrzypak2@gmail.com>
2020-11-27 16:45:44 +0100

    button is disabled when categories == 0

--- lib/pages/create_room/create_room_categories.dart
+++ lib/pages/create_room/create_room_categories.dart
@@ -24,6 +24,8 @@ class _CreateRoomCategoriesState extends State<CreateRoomCategories> {
   Category _selectedCategory;
   ValueObject _selectedValueObject;
   var _isInit = true;
+  var _isButtonDisabled;
+
   List<ValueObject> _dropdownItems = [
     ValueObject(id: 1, value: "1"),
     ValueObject(id: 2, value: "2"),
@@ -35,17 +37,28 @@ class _CreateRoomCategoriesState extends State<CreateRoomCategories> {
   _addCategory(int roomId, int id, int weight) {
     Provider.of<CategoryProvider>(context, listen: false)
         .addCategory(roomId, id, weight);
+    setState(() {
+      _isButtonDisabled = false;
+    });
   }
 
-  _deletePivot(int roomId, int pivotId)
-  {
+  _deletePivot(int roomId, int pivotId) {
     Provider.of<CategoryProvider>(context, listen: false)
         .deletePivot(roomId, pivotId);
+    if (pivots.isEmpty) {
+      setState(() {
+        _isButtonDisabled = true;
+      });
+    }
   }
+
   @override
   void initState() {
+    _isButtonDisabled = true;
     categories =
-        Provider.of<CategoryProvider>(context, listen: false).categories;
+        Provider
+            .of<CategoryProvider>(context, listen: false)
+            .categories;
     _dropdownMenuItems = buildDropDownMenuItems(categories);
     _selectedCategory = _dropdownMenuItems[0].value;
     _dropdownMenuItemsValueObject =
@@ -58,7 +71,10 @@ class _CreateRoomCategoriesState extends State<CreateRoomCategories> {
   @override
   void didChangeDependencies() {
     if (_isInit) {
-      roomId = ModalRoute.of(context).settings.arguments as int;
+      roomId = ModalRoute
+          .of(context)
+          .settings
+          .arguments as int;
     }
     _isInit = false;
     super.didChangeDependencies();
@@ -93,8 +109,9 @@ class _CreateRoomCategoriesState extends State<CreateRoomCategories> {
 
   @override
   Widget build(BuildContext context) {
-    pivots = Provider.of<CategoryProvider>(context, listen: false).pivots ?? [];
-
+    pivots = Provider
+        .of<CategoryProvider>(context, listen: true)
+        .pivots ?? [];
     var createRoom = () {
       final form = formKey.currentState;
       if (form.validate()) {
@@ -114,85 +131,85 @@ class _CreateRoomCategoriesState extends State<CreateRoomCategories> {
           constraints: BoxConstraints.expand(),
           decoration: BoxDecoration(
               gradient: LinearGradient(colors: [
-            Color(ColorsPalette.secondaryColor),
-            Color(ColorsPalette.primaryColor)
-          ], begin: Alignment.topLeft, end: Alignment.bottomRight)),
+                Color(ColorsPalette.secondaryColor),
+                Color(ColorsPalette.primaryColor)
+              ], begin: Alignment.topLeft, end: Alignment.bottomRight)),
           padding: EdgeInsets.all(40.0),
           child: Form(
             key: formKey,
             child: SingleChildScrollView(
               child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                //Center Column contents vertically,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  //Center Column contents vertically,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
                   SizedBox(height: 135.0),
-                  Text(
-                    'Choose category and weight',
-                    style: TextStyle(
-                        fontSize: 26.0,
-                        color: Colors.white,
-                        fontWeight: FontWeight.w900,
-                        fontFamily: 'Playfair Display'),
+              Text(
+                'Choose category and weight',
+                style: TextStyle(
+                    fontSize: 26.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.w900,
+                    fontFamily: 'Playfair Display'),
+              ),
+              SizedBox(height: 30.0),
+              Row(
+                mainAxisSize: MainAxisSize.min, // see 3
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Flexible(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: DropdownButton<Category>(
+                          isExpanded: true,
+                          value: _selectedCategory,
+                          selectedItemBuilder: (BuildContext context) {
+                            return categories
+                                .map<Widget>((Category category) {
+                              return Text(
+                                category.name,
+                                overflow: TextOverflow.ellipsis,
+                                maxLines: 1,
+                                style: TextStyle(color: Colors.white),
+                                softWrap: true,
+                              );
+                            }).toList();
+                          },
+                          items: _dropdownMenuItems,
+                          onChanged: (value) {
+                            setState(() {
+                              _selectedCategory = value;
+                            });
+                          }),
+                    ),
                   ),
-                  SizedBox(height: 30.0),
-                  Row(
-                    mainAxisSize: MainAxisSize.min, // see 3
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Flexible(
-                        child: Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: DropdownButton<Category>(
-                              isExpanded: true,
-                              value: _selectedCategory,
-                              selectedItemBuilder: (BuildContext context) {
-                                return categories
-                                    .map<Widget>((Category category) {
-                                  return Text(
-                                    category.name,
-                                    overflow: TextOverflow.ellipsis,
-                                    maxLines: 1,
-                                    style: TextStyle(color: Colors.white),
-                                    softWrap: true,
-                                  );
-                                }).toList();
-                              },
-                              items: _dropdownMenuItems,
-                              onChanged: (value) {
-                                setState(() {
-                                  _selectedCategory = value;
-                                });
-                              }),
-                        ),
-                      ),
-                      Flexible(
-                        child: Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: DropdownButton<ValueObject>(
-                              value: _selectedValueObject,
-                              selectedItemBuilder: (BuildContext context) {
-                                return _dropdownItems
-                                    .map<Widget>((ValueObject vo) {
-                                  return Text(
-                                    vo.value,
-                                    overflow: TextOverflow.ellipsis,
-                                    maxLines: 1,
-                                    style: TextStyle(color: Colors.white),
-                                    softWrap: false,
-                                  );
-                                }).toList();
-                              },
-                              items: _dropdownMenuItemsValueObject,
-                              onChanged: (value) {
-                                setState(() {
-                                  _selectedValueObject = value;
-                                });
-                              }),
-                        ),
-                      ),
-                      Flexible(
-                          child: Padding(
+                  Flexible(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: DropdownButton<ValueObject>(
+                          value: _selectedValueObject,
+                          selectedItemBuilder: (BuildContext context) {
+                            return _dropdownItems
+                                .map<Widget>((ValueObject vo) {
+                              return Text(
+                                vo.value,
+                                overflow: TextOverflow.ellipsis,
+                                maxLines: 1,
+                                style: TextStyle(color: Colors.white),
+                                softWrap: false,
+                              );
+                            }).toList();
+                          },
+                          items: _dropdownMenuItemsValueObject,
+                          onChanged: (value) {
+                            setState(() {
+                              _selectedValueObject = value;
+                            });
+                          }),
+                    ),
+                  ),
+                  Flexible(
+                    child: Padding(
                         padding: const EdgeInsets.all(8.0),
                         child: IconButton(
                             icon: Icon(Icons.add),
@@ -200,9 +217,10 @@ class _CreateRoomCategoriesState extends State<CreateRoomCategories> {
                             onPressed: () {
                               _addCategory(roomId, _selectedCategory.id,
                                   _selectedValueObject.id);
-                            }),
-                      ))
-                    ],
+
+                            }
+                        ))
+                  )],
                   ),
                   Consumer<CategoryProvider>(
                     builder: (context, pivotProvider, child) {
@@ -241,6 +259,7 @@ class _CreateRoomCategoriesState extends State<CreateRoomCategories> {
                                           color: Colors.white,
                                           onPressed: () {
                                             _deletePivot(roomId, pivot.id);
+
                                           }),
                                     )
                                   ],
@@ -254,16 +273,18 @@ class _CreateRoomCategoriesState extends State<CreateRoomCategories> {
                   SizedBox(height: 120.0),
                   Center(
                     child: RaisedButton(
+
                       elevation: 10,
                       color: Colors.white,
                       padding:
-                          EdgeInsets.symmetric(vertical: 15, horizontal: 50),
+                      EdgeInsets.symmetric(vertical: 15, horizontal: 50),
                       shape: new RoundedRectangleBorder(
                           borderRadius: new BorderRadius.circular(50.0)),
                       child: Text('Next'.toUpperCase(),
                           style: TextStyle(
                               fontSize: 18.0, color: Colors.indigoAccent)),
-                      onPressed: createRoom,
+                      onPressed: pivots.isEmpty ? null : () => createRoom,
+
                     ),
                   ),
                   SizedBox(height: 30.0),
