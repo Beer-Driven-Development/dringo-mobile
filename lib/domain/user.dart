f43795a2fe101c247d0287c04263874a2a1383f3
MattViper <mattoshinigami@gmail.com>
2020-12-08 16:41:24 +0100

    fixed token decoding

--- lib/domain/user.dart
+++ lib/domain/user.dart
@@ -4,27 +4,26 @@ class User {
   String email;
   String token;
 
-  User(
-      {this.id,
-      this.username,
-      this.email,
-      this.token,
-     });
+  User({
+    this.id,
+    this.username,
+    this.email,
+    this.token,
+  });
 
-  factory User.fromToken(String encodedToken, Map<String, dynamic> decodedToken) {
+  factory User.fromToken(
+      String encodedToken, Map<String, dynamic> decodedToken) {
     return User(
-      token: encodedToken,
-      id: decodedToken["user"]["id"],
-      email: decodedToken["user"]["email"],
-      username: decodedToken["user"]["username"]
-    );
+        token: encodedToken,
+        id: decodedToken["id"],
+        email: decodedToken["email"],
+        username: decodedToken["username"]);
   }
 
   factory User.fromJson(Map<String, dynamic> responseData) {
     return User(
-    id: responseData["id"],
-      email: responseData["email"],
-      username: responseData["username"]
-    );
+        id: responseData["id"],
+        email: responseData["email"],
+        username: responseData["username"]);
   }
 }
